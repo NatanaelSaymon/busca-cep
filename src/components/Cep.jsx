1236079c28c839e5e4a633c0e314e310914f0896
Natanael Saymon <saymoneo@gmail.com>
2022-10-08 12:06:29 -0300

    feat: app finalizado

--- src/components/Cep.jsx
+++ src/components/Cep.jsx
@@ -1,25 +1,56 @@
-
+import { useState } from 'react'
 import styles from './Cep.module.css'
 import search from '../assets/icon-search.png'
+import api from '../services/api'
 
 export function Cep() {
+
+    const [cep, setCep] = useState('')
+    const [cepData, setCepData] = useState({})
+
+    async function handleSearchCep() {
+        if(cep === '') {
+            alert('Preencha algum cep')
+            return
+        }
+       
+        try {
+            const response = await api.get(`${item}/json`);
+            setCepData(response.data)
+        }
+
+        catch {
+            alert('Erro ao buscar o CEP')
+            setCep('')
+        }
+    }
+
     return(
         <div className={styles["container"]}>
             <h1 className={styles["title"]}>Buscador de CEP</h1>
-
+            
             <div className={styles["containerInput"]}>
-                <input type="text" className={styles['input']} placeholder="digite seu cep..."/>
-                <button className={styles["buttonSearch"]}>
+                <input 
+                    type="text" 
+                    className={styles['input']} 
+                    placeholder="digite seu cep..."
+                    value={cep}
+                    onChange={(event) => { setCep(event.target.value) }}
+                />
+                
+                <button className={styles["buttonSearch"]} onClick={handleSearchCep}>
                     <img src={search} alt="Icone Search" />
                 </button>
             </div>
 
-            <main className={styles["main"]}>
-                <h2>CEP: 08141140</h2>
-                <span>Endereço: Rua Ilha da Cananeia</span>
-                <span>Bairro: Jardim Indaia</span>
-                <span>Cidade: São Paulo/ SP</span>
-            </main>
+            {Object.keys(cepData).length > 1 && (
+                <main className={styles["main"]}>
+                    <h2>{cepData.cep}</h2>
+                    <span>{cepData.logradouro}</span>
+                    <span>{cepData.bairro}</span>
+                    <span>{cepData.localidade} - {cepData.uf}</span>
+                </main>
+            )}
         </div>
     )
 }
